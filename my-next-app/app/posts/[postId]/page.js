b62654b3443e8c4f427ae2256596d24838a1ac8c
Ayushi <tikoochaiya@gmail.com>
2023-09-30 16:55:41 +0530

    crash course next.js

---
+++ my-next-app/app/posts/[postId]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OnePost = () => {
+    return (
+        <div>{postId}</div>
+    )
+}
+
+export default OnePost
---
+++ my-next-app/app/posts/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// posts
+const Post = () => {
+    return (
+        <div>Hello Post</div>
+    )
+}
+
+export default Post
