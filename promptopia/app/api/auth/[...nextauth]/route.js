9085f539e2ad6f6bde4d8bf65b7df52a04713dbe
Ayushi <tikoochaiya@gmail.com>
2023-10-22 19:36:30 +0530

    changes in next auth

--- promptopia/app/api/auth/[...nextauth]/route.js
+++ promptopia/app/api/auth/[...nextauth]/route.js
@@ -10,36 +10,38 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        })
-
-        session.user.id = sessionUser.id.toString();
-        return session
-    },
-    async signIn({ profile }) {
-        try {
-            // server less route
-            await connectToDB();
-            const userExists = await User.findOne({
-                email: profile.email
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             })
-
-            // if user doesn't exist create a new one
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").
-                    toLowerCase(),
-                    image: profile.picture
+    
+            session.user.id = sessionUser._id.toString();
+            return session
+        },
+        async signIn({ profile }) {
+            try {
+                // server less route
+                await connectToDB();
+                const userExists = await User.findOne({
+                    email: profile.email
                 })
+    
+                // if user doesn't exist create a new one
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").
+                        toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+    
+                return true
+            } catch (error) {
+                console.log("Error in next auth", error)
+                return false
             }
-
-            return true
-        } catch (error) {
-            console.log("Error in next auth", error)
-            return false
         }
     }
 })
